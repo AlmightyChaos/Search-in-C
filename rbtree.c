af330d490d6365cfa765856abb5215b3ca5a5003
Shawn T <tryit223988@gmail.com>
2019-10-12 22:50:07 -0700

    delete, traversal, some other functions are updated

--- rbtree.c
+++ rbtree.c
@@ -32,7 +32,9 @@ bool enqueue(struct node*);
 bool dequeue(struct node**);
 
 int insertNode(int, char*);
-int deleteNode();
+int deleteNode(int);
+struct node* searchNode(int);
+struct node** parentChildCheck(struct node*);
 void leftRotation(struct node *);
 void rightRotation(struct node *);
 void insertFixUp();
@@ -42,8 +44,24 @@ void levelOrderTraversal();
 int main(){
 	insertNode(5, "faker");
 	insertNode(10, "nick");
+	insertNode(2, "fuick");
+	insertNode(8, "fffnick");
+	insertNode(12, "nickfdf");
+	insertNode(4, "nic2223k");
+	insertNode(1, "ni34ck");
+	leftRotation(root);
 	levelOrderTraversal();
+	printf("%s\n", searchNode(10)->data);
+	printf("%ld\n", (parentChildCheck(root->right_child)));
+	printf("%ld\n", &root->right_child);
 	return 0;
+}
+struct node** parentChildCheck(struct node* current){
+	struct node* parent = current->parent;
+	if (parent->left_child == current)
+		return &(parent->left_child);
+	else
+		return &(parent->right_child);
 }
 
 int insertNode(int key, char* data){
@@ -72,30 +90,79 @@ int insertNode(int key, char* data){
 	}
 	new_node->key = key;
 	new_node->data = data;
+	new_node->color = COLOR_RED;
 	new_node->left_child = &nil;
 	new_node->right_child = &nil;
-	nil.parent = new_node;
 	return 0;
+}
+
+int deleteNode(int key){
+	struct node* del_node = searchNode(key);
+	struct node* left_child = del_node->left_child;
+	struct node* right_child = del_node->right_child;
+	struct node* parent = del_node->parent;
+
+	if (del_node != NULL){
+		if (left_child != &nil && right_child != &nil){
+			
+		}
+		else if (left_child != &nil){
+			
+		}
+		else if (right_child != &nil){
+
+		}
+		else{
+			if (parent->left_child == del_node)
+				parent->left_child = &nil;
+			else
+				parent->right_child = &nil;		
+			free(del_node);
+		}
+
+		
+	}
+	else{
+	 	
+		printf("The deleted node doesn't exist!\n");
+		return 1;
+	}
+
+}
+struct node* searchNode(int key){
+	struct node* current = NULL;
+	struct node* next_node = NULL;
+	next_node = root;
+	while(next_node != &nil){
+		current = next_node;
+		if (key != current->key){
+			next_node = key > current->key ? current->right_child : current->left_child;
+		}
+		else
+			return current;
+	}
+	return NULL;
 }
 
 void leftRotation(struct node *current){
 	struct node* right_child = current->right_child;
 	struct node* right_left_child = right_child->left_child;
 	struct node* parent = current->parent;
-	struct node* pars_child = NULL;
 	if (current != root){
 		right_child->parent = parent;
-		pars_child = parent->left_child == current?parent->left_child:parent->right_child;
-		pars_child = right_child;
+		if (parent->left_child == current)
+			parent->left_child = right_child;
+		else
+			parent->right_child = right_child;
 	}
 	else {
 		right_child->parent = NULL;
 		root = right_child;
 	}
 	right_left_child->parent = current;
-	right_left_child = current;
-	parent = right_child;
-	right_child = right_left_child;
+	right_child->left_child = current;
+	current->parent = right_child;
+	current->right_child = right_left_child;
 }
 
 void rightRotation(struct node *current){
