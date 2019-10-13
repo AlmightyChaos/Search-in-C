ae82e37be75d3c7a3664e21f34c63cd87e7d1d9f
Shawn T <tryit223988@gmail.com>
2019-10-13 08:51:26 -0700

    delete node, leftrotation, parentChildCheck are updated!

--- rbtree.c
+++ rbtree.c
@@ -23,7 +23,7 @@ struct node nil={
 	.parent = NULL
 };
 
-struct node *root = NULL;
+struct node *root = &nil;
 struct node *queue[QUEUE_SIZE];
 int front = QUEUE_SIZE - 1;
 int rear = QUEUE_SIZE - 1;
@@ -49,15 +49,23 @@ int main(){
 	insertNode(12, "nickfdf");
 	insertNode(4, "nic2223k");
 	insertNode(1, "ni34ck");
+	insertNode(13, "ytyty77");
 	leftRotation(root);
+	deleteNode(10);
 	levelOrderTraversal();
-	printf("%s\n", searchNode(10)->data);
-	printf("%ld\n", (parentChildCheck(root->right_child)));
-	printf("%ld\n", &root->right_child);
+	//printf("%s\n", searchNode(10)->data);
+	//printf("%ld\n", (root->right_child));
+	//printf("%ld\n", *(struct node **)((void*)root+24));
+	//*(struct node**)((void*)root+24) = root;
+	printf("%ld\n", *parentChildCheck(root));
+	printf("%ld\n", root);
 	return 0;
 }
 struct node** parentChildCheck(struct node* current){
-	struct node* parent = current->parent;
+	struct node* parent = NULL;
+	if (current == root)
+		return &root;
+	parent = current->parent;
 	if (parent->left_child == current)
 		return &(parent->left_child);
 	else
@@ -68,7 +76,7 @@ int insertNode(int key, char* data){
 	struct node* new_node = (struct node*)malloc(sizeof(struct node));
 	struct node* current = NULL;
 	struct node* next_node = NULL;
-	if (root == NULL)
+	if (root == &nil)
 		root = new_node;
 	else{
 		next_node = root;
@@ -83,10 +91,13 @@ int insertNode(int key, char* data){
 			}
 		}
 		new_node->parent = current;
+
 		if (key > current->key) 
 			current->right_child = new_node;
 		else
 			current->left_child = new_node;
+
+
 	}
 	new_node->key = key;
 	new_node->data = data;
@@ -100,30 +111,40 @@ int deleteNode(int key){
 	struct node* del_node = searchNode(key);
 	struct node* left_child = del_node->left_child;
 	struct node* right_child = del_node->right_child;
-	struct node* parent = del_node->parent;
 
 	if (del_node != NULL){
 		if (left_child != &nil && right_child != &nil){
-			
+			//In this situation, one can either choose to go left or right at first step.
+			//We go right here.
+			while(right_child->left_child != &nil)
+				right_child = right_child->left_child;
+			//Copy the key & data from subtitute to del_node.
+			del_node->key = right_child->key;
+			del_node->data = right_child->data;
+			//Tree links to the leaf.
+			*parentChildCheck(right_child) = right_child->right_child;
+			//Leaf links back to the tree.
+			right_child->right_child->parent = right_child->parent;
+			//Exchange and let the substitute go to hell.
+			del_node = right_child;
 		}
 		else if (left_child != &nil){
-			
+			*parentChildCheck(del_node) = left_child;
+			left_child->parent = (del_node != root)?del_node->parent:NULL;
 		}
 		else if (right_child != &nil){
-
+			*parentChildCheck(del_node) = right_child;
+			right_child->parent = (del_node != root)?del_node->parent:NULL;
 		}
 		else{
-			if (parent->left_child == del_node)
-				parent->left_child = &nil;
-			else
-				parent->right_child = &nil;		
-			free(del_node);
+			*parentChildCheck(del_node) = &nil;
 		}
 
-		
+		free(del_node);
+
 	}
 	else{
-	 	
+
 		printf("The deleted node doesn't exist!\n");
 		return 1;
 	}
@@ -147,18 +168,25 @@ struct node* searchNode(int key){
 void leftRotation(struct node *current){
 	struct node* right_child = current->right_child;
 	struct node* right_left_child = right_child->left_child;
-	struct node* parent = current->parent;
-	if (current != root){
-		right_child->parent = parent;
-		if (parent->left_child == current)
-			parent->left_child = right_child;
-		else
-			parent->right_child = right_child;
-	}
-	else {
-		right_child->parent = NULL;
-		root = right_child;
-	}
+	/*
+	   if (current != root){
+
+	   right_child->parent = current->parent;
+
+	   if (parent->left_child == current)
+	   parent->left_child = right_child;
+	   else
+	   parent->right_child = right_child;
+
+	 *parentChildCheck(current) = right_child;
+	 }
+	 else {
+	 right_child->parent = NULL;
+	 root = right_child;
+	 }
+	 */
+	right_child->parent = (current!=root)?current->parent:NULL;
+	*parentChildCheck(current) = right_child;
 	right_left_child->parent = current;
 	right_child->left_child = current;
 	current->parent = right_child;
