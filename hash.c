260c2b6b079e034d01bc4005fd660878ea80704b
AlmightyChaos <tryit223988@gmail.com>
2019-11-29 23:39:24 +0800

    hash is updated

---
+++ hash.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *hash_table;
+
+void hashInit(unsigned int bits){
+	hash_table = (int *)malloc(sizeof(int)*bits);
+}
+
+void hashAdd(){
+
+}
+
+void hashDel(){
+
+}
+
+void hashSearch(){
+
+}
+
+int stringPreHash(){
+
+}
+
+void hashFunction(){
+
+
+}
+
+void chaining(){
+
+}
+
+void reHash(){
+
+}
+
+void loadFactor(){
+
+}
+
+void tranToRbtree(){
+
+}
+
+void tranToList(){
+
+}
+
+int main(){
+	hashInit(3);
+	return 0;
+}
